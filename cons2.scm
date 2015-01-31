7e3a5a372cd66552b4eefe2f5d8695aa10ae2626
Bruce Richardson <it.is.bruce@gmail.com>
2015-01-31 06:51:01 +0000

    cons (2): insertR

--- cons2.scm
+++ cons2.scm
@@ -10,3 +10,10 @@
     (cond
       ((null? l) l)
       ((cons (car (car l)) (firsts (cdr l)))) )))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) lat)
+      ((eq? old (car lat)) (cons old (cons new (cdr lat))))
+      (else (cons (car lat) (insertR new old (cdr lat)))) )))
