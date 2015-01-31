14215141008e0d3be71ec939b4658ed4360863b4
Bruce Richardson <it.is.bruce@gmail.com>
2015-01-31 00:46:59 +0000

    Redid exercises from again

---
+++ again2.scm
@@ -0,0 +1,19 @@
+(define atom?
+  (lambda (a)
+    (and (not (null? a)) (not (pair? a))) ))
+
+(define lat?
+  (lambda (l)
+    (or
+      (null? l)
+      (and
+	(not (atom? l))
+	(atom? (car l))
+	(lat? (cdr l)) ))))
+
+(define member?
+  (lambda (a l)
+    (cond 
+      ((null? l) #f)
+      ((eq? a (car l)) #t)
+      (else (member? a (cdr l))) )))
