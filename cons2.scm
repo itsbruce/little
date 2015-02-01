6281f0689ffc6bf823015db689ee0935ef571c7b
Bruce Richardson <it.is.bruce@gmail.com>
2015-02-01 12:53:44 +0000

    cons (2): insertL, subst, subst2

--- cons2.scm
+++ cons2.scm
@@ -17,3 +17,24 @@
       ((null? lat) lat)
       ((eq? old (car lat)) (cons old (cons new (cdr lat))))
       (else (cons (car lat) (insertR new old (cdr lat)))) )))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? old (car lat)) (cons new (cons old(cdr lat))))
+      (else (cons (car lat) (insertL new old (cdr lat)))) )))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? old (car lat)) (cons new lat))
+      (else (cons (car lat) (subst new old (cdr lat)))) )))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond 
+      ((null? lat) '())
+      ((or (eq? o1 (car lat)) (eq? o2 (car lat))) (cons new (cdr lat)))
+      (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))) )))
