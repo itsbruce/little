0d610db72ac5c670abaadbeef036245db6def020
Bruce Richardson <it.is.bruce@gmail.com>
2015-02-01 14:36:05 +0000

    cons (2): multirember multiinsertR

--- cons2.scm
+++ cons2.scm
@@ -40,3 +40,18 @@
       ((null? lat) '())
       ((or (eq? o1 (car lat)) (eq? o2 (car lat))) (cons new (cdr lat)))
       (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))) )))
+
+(define multirember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? a (car lat)) (multirember a (cdr lat)))
+      (else (cons (car lat) (multirember a (cdr lat)))) )))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else (cons (car lat) (cond
+                        ((eq? old (car lat)) (cons new (multiinsertR new old (cdr lat))))
+                        (else (multiinsertR new old (cdr lat))) ))))))
